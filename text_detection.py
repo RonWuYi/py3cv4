6036f5f1ca1b7d2a1956ae4349d4371ab7d1bee6
RonWuYi <lyshmily@outlook.com>
2019-06-15 03:31:30 -0700

    update func

--- text_detection.py
+++ text_detection.py
@@ -6,14 +6,13 @@ import cv2
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", type=str, help="path to input image")
-ap.add_argument("-east", "--east", type=str,
-	            help="path to input EAST text detector")
+ap.add_argument("-east", "--east", type=str, help="path to input EAST text detector")
 ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
-	            help="minimum probability required to inspect a region")
+                help="minimum probability required to inspect a region")
 ap.add_argument("-w", "--width", type=int, default=320,
-	            help="resized image width (should be multiple of 32)")
+                help="resized image width (should be multiple of 32)")
 ap.add_argument("-e", "--height", type=int, default=320,
-	            help="resized image height (should be multiple of 32)")
+                help="resized image height (should be multiple of 32)")
 args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 # if image is None:
