a49c1cee0dabe21c619d445b1e0ad741b66624be
RonWuYi <lyshmily@outlook.com>
2019-06-15 03:58:15 -0700

    update name and cp area

--- test001.py
+++ test001.py
@@ -6,3 +6,10 @@ x = re.search(r"(\d+)", myString)
 # result =x.match(myString)
 print(x.group(0))
 print(type(x.group(0)))
+
+##### this is name area ###################
+startX, startY, endX, endY = 242, 513, 396, 570
+
+
+# this is cp area
+startX, startY, endX, endY = 272, 63, 366, 140
