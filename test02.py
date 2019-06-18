c16275b8ca7cd4aba1e86c1c53d76b86a78cc860
RonWuYi <lyshmily@outlook.com>
2019-06-18 15:47:40 +0200

    update db func

--- test02.py
+++ test02.py
@@ -1,13 +1,16 @@
-import cv2
-import pytesseract
 import os
+import re
+import cv2
+import psycopg2
+import pytesseract
 import xml.etree.ElementTree as ET
 
-my_xml = '/home/hdc/PycharmProjects/py3cv4/xml/result.xml'
+my_xml = '/home/hdc/Downloads/project/py3cv4/xml/result.xml'
+
 three = ET.parse(my_xml)
 root = three.getroot()
 
-root_folder = '/home/hdc/PycharmProjects/py3cv4/png/'
+root_folder = '/home/hdc/Downloads/project/py3cv4/png'
 config = "-l eng --oem 1 --psm 3"
 
 fix_box2 = [(289, 65, 366, 129), (232, 513, 393, 570), (259, 593, 298, 620), (353, 905, 419, 940)]
@@ -18,6 +21,8 @@ name, cp, hp, dust = None, None, None, None
 
 for x, _, z in os.walk(root_folder):
     if len(z) > 0:
+        conn = psycopg2.connect(database="testdb", user="postgres", password="postgres", host="172.16.66.244")
+        cur = conn.cursor()
         for i in z:
             if x[-1:] == "2":
                 img = cv2.imread(os.path.join(x, i))
@@ -27,17 +32,18 @@ for x, _, z in os.walk(root_folder):
                     # cv2.imshow("origional", orig)
                     roi = orig[startY:endY, startX:endX]
                     text = pytesseract.image_to_string(roi, config=config)
-                    print(text)
+                    # print(text)
                     # print(pytesseract.image_to_string(Image.fromarray(roi)))
                     # cv2.waitKey(0)
                     if fix_box2.index((startX, startY, endX, endY)) == 0:
-                        cp = text
+                        # cp = text
+                        cp = re.search(r'[0-9]*', text).group(0)
                     elif fix_box2.index((startX, startY, endX, endY)) == 1:
-                        name = text
+                        name = re.search(r'[a-z.A-Z]*', text).group(0)
                     elif fix_box2.index((startX, startY, endX, endY)) == 2:
-                        hp = text
+                        hp = re.search(r'[0-9]*', text).group(0)
                     else:
-                        dust = text
+                        dust = re.search(r'[0-9]*', text).group(0)
                 bug = ET.SubElement(root, 'bug')
                 bug_name = ET.SubElement(bug, 'name')
                 bug_name.text = name
@@ -47,24 +53,26 @@ for x, _, z in os.walk(root_folder):
                 bug_hp.text = hp
                 bug_dust = ET.SubElement(bug, 'dust')
                 bug_dust.text = dust
+                cur.execute("INSERT INTO bugs (name, cp, hp, dust) VALUES (%s, %s, %s, %s)",
+                            (name, int(cp), int(hp), int(dust)))
             elif x[-1:] == "3":
                 img = cv2.imread(os.path.join(x, i))
                 orig = img.copy()
                 for (startX, startY, endX, endY) in fix_box3:
                     # cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
                     # cv2.imshow("origional", orig)
-
                     roi = orig[startY:endY, startX:endX]
                     text = pytesseract.image_to_string(roi, config=config)
-                    print(text)
+                    # print(text)
                     if fix_box3.index((startX, startY, endX, endY)) == 0:
-                        cp = text
+                        # cp = text
+                        cp = re.search(r'[0-9]*', text).group(0)
                     elif fix_box3.index((startX, startY, endX, endY)) == 1:
-                        name = text
+                        name = re.search(r'[a-z.A-Z]*', text).group(0)
                     elif fix_box3.index((startX, startY, endX, endY)) == 2:
-                        hp = text
+                        hp = re.search(r'[0-9]*', text).group(0)
                     else:
-                        dust = text
+                        dust = re.search(r'[0-9]*', text).group(0)
                     # cv2.waitKey(0)
                 bug = ET.SubElement(root, 'bug')
                 bug_name = ET.SubElement(bug, 'name')
@@ -75,6 +83,8 @@ for x, _, z in os.walk(root_folder):
                 bug_hp.text = hp
                 bug_dust = ET.SubElement(bug, 'dust')
                 bug_dust.text = dust
+                cur.execute("INSERT INTO bugs (name, cp, hp, dust) VALUES (%s, %s, %s, %s)",
+                            (name, int(cp), int(hp), int(dust)))
             else:
                 img = cv2.imread(os.path.join(x, i))
                 orig = img.copy()
@@ -83,15 +93,16 @@ for x, _, z in os.walk(root_folder):
                     # cv2.imshow("origional", orig)
                     roi = orig[startY:endY, startX:endX]
                     text = pytesseract.image_to_string(roi, config=config)
-                    print(text)
+                    # print(text)
                     if fix_box4.index((startX, startY, endX, endY)) == 0:
-                        cp = text
+                        # cp = text
+                        cp = re.search(r'[0-9]*', text).group(0)
                     elif fix_box4.index((startX, startY, endX, endY)) == 1:
-                        name = text
+                        name = re.search(r'[a-z.A-Z]*', text).group(0)
                     elif fix_box4.index((startX, startY, endX, endY)) == 2:
-                        hp = text
+                        hp = re.search(r'[0-9]*', text).group(0)
                     else:
-                        dust = text
+                        dust = re.search(r'[0-9]*', text).group(0)
                     # cv2.waitKey(0)
                 bug = ET.SubElement(root, 'bug')
                 bug_name = ET.SubElement(bug, 'name')
@@ -102,6 +113,11 @@ for x, _, z in os.walk(root_folder):
                 bug_hp.text = hp
                 bug_dust = ET.SubElement(bug, 'dust')
                 bug_dust.text = dust
+                cur.execute("INSERT INTO bugs (name, cp, hp, dust) VALUES (%s, %s, %s, %s)",
+                            (name, int(cp), int(hp), int(dust)))
+        conn.commit()
+        conn.close()
 
 tree = ET.ElementTree(root)
 tree.write("output.xml")
+print("Done!")
--- test13.py
+++ test13.py
@@ -1,4 +1,17 @@
-my_list = ['a', 'b', 'c']
+from pathlib import Path
 
+import psycopg2
 
-print(my_list.index('a'))
+conn = psycopg2.connect(database="testdb", user="postgres",
+                        password="postgres", host="172.16.66.244")
+
+cur = conn.cursor()
+try:
+    cur.execute("SELECT * from test;")
+    test = cur.fetchone()
+    print(cur.fetchone())
+    print(type(cur.fetchone()))
+    print(type(test))
+except AttributeError as e:
+    print(e)
+# print(test.fetchone())
