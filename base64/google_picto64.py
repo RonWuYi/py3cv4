165b59bcc2ea7b8971a011633f14b377adcf2907
RonWuYi <lyshmily@outlook.com>
2019-06-24 09:51:18 +0200

    add google cloud

--- base64/google_picto64.py
+++
@@ -1,4 +0,0 @@
-import os
-# from google.cloud
-def detect_document(path):
-    from google.cloud import vision
--- test14.py
+++ test14.py
@@ -1,1 +1,6 @@
-pass
+import os
+
+key_path = "/home/hdc/Downloads/project/py3cv4/jsonKey/lyshmily-457c5c843982.json"
+os.system('export GOOGLE_APPLICATION_CREDENTIALS={}'.format(key_path))
+
+print(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS '))
