73b568f871161d58af274567677b164a862c7884
RonWuYi <lyshmily@outlook.com>
2019-06-24 17:13:52 +0200

    add google cloud

--- googlebase64/picto64.py
+++ googlebase64/picto64.py
@@ -3,13 +3,21 @@ import base64
 
 file_folder = '/home/hdc/Downloads/project/py3cv4/png/2'
 
-
+test_file = '/home/hdc/Downloads/project/py3cv4/png/2/IMG_5183.PNG'
 # for i in os.listdir(file_folder):
 #     print(i)
 
-for i in os.listdir(file_folder):
-    with open(os.path.join(file_folder, i), 'rb') as image_file:
-        encode_string = base64.b64encode(image_file.read())
-        print("this is file {}".format(i))
-        print(encode_string)
-        print("################################")
+# for i in os.listdir(file_folder):
+#     with open(os.path.join(file_folder, i), 'rb') as image_file:
+#         encode_string = base64.b64encode(image_file.read())
+#         print("this is file {}".format(i))
+#         print(encode_string)
+#         print("################################")
+
+with open(test_file, 'rb') as image_file:
+    encode_string = base64.b64encode(image_file.read())
+    print("this is file {}".format(image_file.name))
+    print(encode_string)
+    print("################################")
+    print(type(encode_string))
+    print(encode_string.decode("utf-8"))
--- test14.py
+++ test14.py
@@ -1,6 +1,11 @@
 import os
+import subprocess
 
 key_path = "/home/hdc/Downloads/project/py3cv4/jsonKey/lyshmily-457c5c843982.json"
-os.system('export GOOGLE_APPLICATION_CREDENTIALS={}'.format(key_path))
 
+
+# os.system('export GOOGLE_APPLICATION_CREDENTIALS={}'.format(key_path))
+
+subprocess.run(["export GOOGLE_APPLICATION_CREDENTIALS={}".format(key_path)], stdout=subprocess.PIPE)
+# print(stdout)
 print(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS '))
